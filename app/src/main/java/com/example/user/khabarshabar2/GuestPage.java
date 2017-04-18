0ece158ee74d1f50f51251f65bdb075c5c1f3145
SamaSamrin <samasamrin@gmail.com>
2017-04-18 17:28:30 +0600

    set up guest page

--- app/src/main/java/com/example/user/khabarshabar2/GuestPage.java
+++ app/src/main/java/com/example/user/khabarshabar2/GuestPage.java
@@ -1,13 +1,106 @@
 package com.example.user.khabarshabar2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.IdRes;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GuestPage extends Activity {
+
+    private final String TAG = "**GuestPage**";
+
+    private TextView chosenAgeDisplay;
+    private SeekBar ageSeekBar;
+    private RadioGroup genderRadioGroup;
+    private EditText heightFeetInput;
+    private EditText heightInchInput;
+    private EditText weightInput;
+
+    String chosenGender = "default gender";
+    int chosenAge = 0;
+    int heightFeet = 0;
+    int heightInch = 0;
+    int weight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_page);
+
+        genderRadioGroup = (RadioGroup) findViewById(R.id.genderGroup);
+
+        chosenAgeDisplay = (TextView) findViewById(R.id.chosenAgeDisplay);
+        ageSeekBar = (SeekBar) findViewById(R.id.ageProgressBar);
+        showChosenAge();
+
+        heightFeetInput = (EditText) findViewById(R.id.feetInput);
+        heightInchInput = (EditText) findViewById(R.id.inchInput);
+        weightInput = (EditText) findViewById(R.id.weightInput);
+        setAllOnClickListeners();
+    }
+
+    private void showChosenAge(){
+        ageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                String currentAgeString = String.valueOf(i);
+                chosenAgeDisplay.setText(currentAgeString);
+                chosenAge = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void onSelectedRadioButton(View v){
+
+        RadioButton selectedGender = (RadioButton) v;
+        boolean selected = selectedGender.isChecked();
+
+        switch (v.getId()){
+            case R.id.male:
+                if(selected){
+                    chosenGender = selectedGender.getText().toString();
+                }
+                break;
+            case R.id.female:
+                if(selected){
+                    chosenGender = selectedGender.getText().toString();
+                }
+                break;
+        }
+        Log.e(TAG, chosenGender);//works
+        //Toast.makeText(this, chosenGender, Toast.LENGTH_LONG).show();//works
+    }
+
+    private void setAllOnClickListeners(){
+
+
+
+    }
+
+    public void goToResultPage(View v){
+        Intent i = new Intent(GuestPage.this, ResultPage.class);
+        i.putExtra("gender", chosenGender);
+        i.putExtra("age", chosenAge);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/user/khabarshabar2/ResultPage.java
@@ -0,0 +1,18 @@
+package com.example.user.khabarshabar2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+public class ResultPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_page);
+
+        Intent i = getIntent();
+
+    }
+}
