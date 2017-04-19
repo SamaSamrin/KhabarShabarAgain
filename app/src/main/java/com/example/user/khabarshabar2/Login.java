87744a4fb27dc57df50a9556842b5a0a6162f8d6
Sama Samrin <samasamrin@gmail.com>
2017-04-20 00:46:17 +0600

    fixed app name and started implementing database

--- app/src/main/java/com/example/user/khabarshabar2/Login.java
+++ app/src/main/java/com/example/user/khabarshabar2/Login.java
@@ -1,21 +1,52 @@
 package com.example.user.khabarshabar2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class Login extends Activity {
 
-    String emailInput;
-    String passwordInput;
-    String username = "Sama Samrin";
+    private final String TAG = "**Login**";
+
+    String emailInput = "default@email.com";
+    String passwordInput = "123456";
+    String username = "default name";
+    static SQLiteDatabase database;
+    String dbname = "DietBossDB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        String datapath = Environment.getDataDirectory().toString();
+        String appname = getApplicationName(this);
+        Log.e(TAG, "app name = "+appname);//DietBoss
+        String dbpath = datapath+"/data/"+appname+"/databases/"+dbname;
+        //database = openOrCreateDatabase(dbpath, MODE_PRIVATE, null);
+        //^ not working, needs root privileges
+        Log.e(TAG, "db path = "+dbpath);
+    }
+
+    public static String getApplicationName(Context context){
+        String appname = "";
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        if(stringId==0)
+            appname = applicationInfo.nonLocalizedLabel.toString();
+        else
+            appname = context.getString(stringId);
+        return appname;
     }
 
     public void goOnClick(View view){
